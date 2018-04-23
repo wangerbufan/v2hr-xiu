68ad76cbecee706d060bc4d073372f5f1808c5d8
zusrt <32688751+zusrt@users.noreply.github.com>
2018-04-23 14:16:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,30 @@
-cd /v2raybin
-if [ ! -f /v2raybin/v2ray-v$VER-linux-64 ]; then
+if [ ! -f /v2raybin/v2ray-v$V2RAY_VER-linux-64 ]; then
   rm -rf /v2raybin
-  wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+  cd /v2raybin
+  wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$V2RAY_VER/v2ray-linux-64.zip
   unzip v2ray.zip 
-  cd /v2raybin/v2ray-v$VER-linux-64
+  cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
   chmod +x v2ray
   chmod +x v2ctl
 fi
 
+if [ ! -f /caddybin/caddy_v$CADDY_VER ]; then
+  rm -rf /caddybin
+  mkdir /caddybin/caddy_v$CADDY_VER
+  cd /caddybin/caddy_v$CADDY_VER
+  wget -O caddy.tar.gz https://github.com/mholt/caddy/releases/download/v0.10.13/caddy_v$CADDY_VER_linux_amd64.tar.gz
+  tar -zxvf caddy.tar.gz 
+  chmod +x caddy
+fi
+
+cd /caddybin/caddy_v$CADDY_VER
+echo 0.0.0.0:$PORT > HerokuCaddyfile
+./caddy -conf="HerokuCaddyfile"
+
+cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
-
 ./v2ray
