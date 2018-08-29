ea2fbb7591682cb75866f02d3a4536fe58a225da
sstutanotam <40928227+sstutanotam@users.noreply.github.com>
2018-08-29 20:59:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,3 @@
-if [ ! -d /v2raybin/v2ray-v$V2RAY_VER-linux-64 ]; then
-  rm -rf /v2raybin
-  mkdir /v2raybin
-  cd /v2raybin
-  wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$V2RAY_VER/v2ray-linux-64.zip
-  unzip v2ray.zip 
-  cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
-  chmod +x v2ray
-  chmod +x v2ctl
-fi
-
 if [ ! -d /caddybin/caddy_v$CADDY_VER ]; then
   rm -rf /caddybin
   mkdir /caddybin
@@ -25,17 +14,6 @@ if [ ! -d $CADDY_ROOT ]; then
   wget -O wallet.bitshares.org-gh-pages.zip https://github.com/bitshares/wallet.bitshares.org/archive/gh-pages.zip
   unzip wallet.bitshares.org-gh-pages.zip
 fi
-
-cd /v2raybin/v2ray-v$V2RAY_VER-linux-64
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$V2_WS_PORT,\"listen\":\"$V2_WS_IP\"" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-echo -e -n "$V2_WS_PATH" >> config.json
-echo -e -n "$CONFIG_JSON4" >> config.json
-echo -e -n "$CONFIG_JSON5" >> config.json
-./v2ray &
 
 cd /caddybin/caddy_v$CADDY_VER
 echo $CADDY_LS_IP:$PORT { > HerokuCaddyfile
